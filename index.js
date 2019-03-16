6e131786f0f520081f59359de896c1e386c7004d
sakal <sakals@mail.ua>
2019-03-16 17:23:48 -0400

    cleanup
    Signed-off-by: sakal <sakals@mail.ua>

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 import isSocket from './isSocket';
 
 
-function socketAuth(ref) {
-  const {dispatch} = ref;
-
+function socketAuth() {
   return next => action => {
-
-    // Instantiate and define constants for action type
-    const TYPE = action.type;
 
     // Is there a payload?
     if (action.payload) {
+
+      // Instantiate and define constants for action type and payload:
+      const TYPE = action.type;
       const PAYLOAD = action.payload;
 
       // Is the socket.io defined?
@@ -36,71 +34,80 @@ function socketAuth(ref) {
 
         // Function: handleConnected
         // This function dispatches the connected action
-        // where payload is connected socket.io
+        // where payload is connected socket.io.
         const handleConnected = () => {
-          return next({
-            type: TYPE + CONNECTED_SUFFIX, // // Concatenate the type string property.
+          next({
+            // Concatenate the type string property.
+            type: TYPE + CONNECTED_SUFFIX,
             payload: socket,
           });
         };
 
-        // Function: handleRejected
-        // This function dispatches the rejected connection action
-        // where payload is error rejection
+        /*  Function: handleRejected connection
+        * This function move into the next middleware
+        * where payload is error rejection
+        * and close/disconnect socket.io.
+        */
         const handleRejected = (error) => {
           socket.close(); // socket.disconnect();
-          return next({
+
+          next({
+            // Concatenate the type string property.
             type: TYPE + REJECTED_SUFFIX,
             payload: error,
           });
         };
 
-        // Function: disconnectedServer
-        // This function dispatches the disconnected server action
-        // where payload is reason disconnecting
-        // and close/disconnect socket.io
+        /*  Function: disconnectedServer connection
+        * This function move into the next middleware
+        * where payload is reason disconnecting
+        * and close/disconnect socket.io.
+        */
         const disconnectedServer = (reason) => {
           socket.close(); // socket.disconnect();
 
-          return next({
+          next({
+            // Concatenate the type string property.
             type: TYPE + DISCONNECTED_SUFFIX,
             payload: reason,
           });
         };
 
-        // Is socket.io in variable
+        // If socket variable hold socket.io?
         if (socket) {
 
-          // Remove previous listeners
+          // Remove listeners:
           socket.removeListener(CONNECT, handleConnected);
           socket.removeListener(ERROR, handleRejected);
           socket.removeListener(CONNECT_ERROR, handleRejected);
           socket.removeListener(CONNECT_TIMEOUT, handleRejected);
           socket.removeListener(DISCONNECT, disconnectedServer);
 
-          // Move on to next action
-          //next(action);
-
-          // Save socket.io into variable
+          // If socket variable not hold socket.io
         } else {
+
+          // Save socket.io into variable.
           socket = PAYLOAD;
 
-          // First, dispatch the pending action:
-          next({
-            type: TYPE + PENDING_SUFFIX,
-          });
-
-          // Second, add listeners and dispatch a rejected or connect or disconnect action
-          // and move on to the next middleware.
+          // First, adding listeners to move into the next middleware.
           socket.on(CONNECT, handleConnected);
           socket.on(ERROR, handleRejected);
           socket.on(CONNECT_ERROR, handleRejected);
           socket.on(CONNECT_TIMEOUT, handleRejected);
           socket.on(DISCONNECT, disconnectedServer);
         }
+
+        // Second, move into the next middleware as pending connection
+        next({
+          type: TYPE + PENDING_SUFFIX,
+        });
+
+        // If payload not socket.io, move into the next middleware.
+      } else {
+        next(action);
       }
 
-      // If there's no payload, move on to the next middleware.
+      // If there's no payload, move into the next middleware.
     } else {
       next(action);
     }
