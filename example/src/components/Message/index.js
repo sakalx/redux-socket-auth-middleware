f236e9d29fa66c58baaced7024a346ae9861526b
sakal <sakals@mail.ua>
2019-03-21 19:13:49 -0400

    feat: user name into message
    Signed-off-by: sakal <sakals@mail.ua>

--- example/src/components/Message/index.js
+++ example/src/components/Message/index.js
@@ -6,13 +6,16 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Slide from '@material-ui/core/Slide';
+import Typography from '@material-ui/core/Typography';
 
 import UserAvatar from '../Avatar';
 
 import {Container} from './style';
 
 const Message = ({message, userId, users}) => {
+  const user = users.data[userId];
   const isAuthor = users.current.id === userId;
+  const colorUserName = user.avatarColor;
 
   return (
       <Slide
@@ -25,7 +28,18 @@ const Message = ({message, userId, users}) => {
             <ListItemAvatar>
               <UserAvatar userId={userId}/>
             </ListItemAvatar>
-            <ListItemText primary={message}/>
+
+            <div>
+              <Typography
+                  component='h3'
+                  style={{color: colorUserName}}
+                  variant='button'
+              >
+                {user.name}
+              </Typography>
+              <ListItemText primary={message}/>
+            </div>
+
           </ListItem>
         </Container>
       </Slide>
