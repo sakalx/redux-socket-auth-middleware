3c73c48d3b1a64036819ec40556459e91c7d189a
sakal <sakals@mail.ua>
2019-03-23 20:52:40 -0400

    chore: move example to example-chat branch
    Signed-off-by: sakal <sakals@mail.ua>

--- example/client/src/api/socket/index.js
+++
@@ -1,8 +0,0 @@
-import socketIOClient from 'socket.io-client';
-
-const url = 'http://localhost:8000';
-
-const socketConnect = user =>
-    socketIOClient.connect(url, {query: `user=${user}`});
-
-export default socketConnect;
--- example/server/socket/middlewares/auth/parseUser.js
+++
@@ -1,9 +0,0 @@
-function parseUser(user, next) {
-  try {
-    return JSON.parse(user);
-  } catch (e) {
-    next(new Error('Not valid user access'));
-  }
-}
-
-module.exports = parseUser;
